0259f63c8ed0d294825e79b8b0b820619e238d9d
杜宗锋 <429147359@qq.com>
2020-05-08 06:36:16 +0800

    注册中心

--- my-project-regist-service/src/main/java/com/qf/my/project/regist/service/controller/RegistController.java
+++ my-project-regist-service/src/main/java/com/qf/my/project/regist/service/controller/RegistController.java
@@ -3,8 +3,8 @@ package com.qf.my.project.regist.service.controller;
 import com.qf.dto.ResultBean;
 import com.qf.my.project.regist.service.service.IRegistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +14,8 @@ public class RegistController {
     @Autowired
     private IRegistService registService;
 
-    @RequestMapping("regist")
-    public ResultBean regist(@RequestParam String uname, @RequestParam String password){
+    @RequestMapping("regist/{uname}/{password}")
+    public ResultBean regist(@PathVariable String uname, @PathVariable String password){
         return registService.regist(uname,password);
     }
 
